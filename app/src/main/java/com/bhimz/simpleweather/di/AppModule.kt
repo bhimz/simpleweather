5c132cd2a74e9bbadcdb8e89f5cc05a93963d8fe
bhimz <bhimz2001@gmail.com>
2019-10-29 10:36:45 +0700

    added Places API

--- app/src/main/java/com/bhimz/simpleweather/di/AppModule.kt
+++ app/src/main/java/com/bhimz/simpleweather/di/AppModule.kt
@@ -1,15 +1,23 @@
 package com.bhimz.simpleweather.di
 
+import com.bhimz.simpleweather.BuildConfig
 import com.bhimz.simpleweather.domain.WeatherApi
 import com.bhimz.simpleweather.domain.service.WeatherService
+import com.google.android.libraries.places.api.Places
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 val appModule = module {
+    single {
+        val ctx = androidContext()
+        Places.initialize(ctx, BuildConfig.PLACES_API_KEY)
+        Places.createClient(ctx)
+    }
     single<Interceptor> {
         val interceptor = HttpLoggingInterceptor()
         interceptor.level = HttpLoggingInterceptor.Level.HEADERS
